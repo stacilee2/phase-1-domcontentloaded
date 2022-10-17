a8e8e85cadb55941e91c2ec702142d5c54006050
Staci Rosales <stacileep2@gmail.com>
2022-10-17 10:04:27 -0500

    commit tree 2f15c12a595a2399d43d7e2aa67c626ffb9ba668

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const message = document.getElementById("text").innerHTML = "This is really cool!";
+    return message;
+});
